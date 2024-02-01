1a66ace6a75a736336c22d34fd717507c5525b36
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-01 14:12:06 -0500

    Complete Lab 01

--- Lab01/fizzbuzz.c
+++ Lab01/fizzbuzz.c
@@ -1,36 +1,23 @@
+//Aaron Nguyen - CS 355-02
+
 #include <stdio.h>
+
+int fizzBuzz(int);
 
 int main() {
     int iNum = 0;
-    printf("Please enter an integer: "); 
-    scanf("%d", &iNum); 
-    if (iNum >=0) { 
-        for (int i = 0; i <= iNum; i++) {
-            if (i % 3 == 0) {
-                if (i % 5 == 0)
-                    printf("FizzBuzz\n");
-                else 
-                    printf("Fizz\n");
-            } 
-            else if (i % 5 == 0)
-                printf("Buzz\n"); 
-            else 
-                printf("%d\n", i);
-        }
-    }
-    else {
-        for (int i = 0; i >= iNum; i--) {
-            if (i % 3 == 0) {
-                if (i % 5 == 0)
-                    printf("FizzBuzz\n");
-                else 
-                    printf("Fizz\n");
-            } 
-            else if (i % 5 == 0)
-                printf("Buzz\n"); 
-            else 
-                printf("%d\n", i);
-        }
-    }
+    printf("Please enter an integer: ");
+    scanf("%d", &iNum);
+    
+    for (int i = 0; (iNum>=0) ? i<=iNum : i>=iNum; (iNum>=0) ? i++ : i--)
+        fizzBuzz(i);
+    return 0;
+}
+
+int fizzBuzz(int i) {
+    if (i % 3 == 0) printf("Fizz");
+    if (i % 5 == 0) printf("Buzz");
+    if (i % 3 != 0 && i % 5 != 0) printf("%d", i);
+    printf("\n");
     return 0;
 }
