e1920ab9e4a6893a4b430a2f103e5f545cea7168
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-13 22:08:10 -0500

    small improvement on Lab02

--- Lab02/myprog.c
+++ Lab02/myprog.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-int checkChar(char c) {
-  if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-    return 1;
-  else
-    return 0;
+int isLetter(char c) {
+  return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 void printArg(char *str) {
   for (int i = 0; str[i] != 0; i++) {
-    // printf("%c - ", str[i]);
-    if (checkChar(str[i])) {
+    if (isLetter(str[i])) {
       for (int j = 0; j <= i; j++) {
-        if (checkChar(str[j]))
+        if (isLetter(str[j]))
           printf("%c", str[j]);
       }
       printf("\n");
