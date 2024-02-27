e8f382aaae7821c24b2660f93bb15ddbd131d23d
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-27 13:55:45 -0500

    Working on Lab03

--- Lab03/mywho.c
+++ Lab03/mywho.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <utmp.h>
+#include <unistd.h>
 
-int main() {}
+int main() {
+    int intUTMPfile;
+    struct utmp currentRecord;
+    int intUTMPlength = sizeof(currentRecord);
+    intUTMPfile = open(UTMP_FILE, O_RDONLY);
+
+    while (read(intUTMPfile, &currentRecord, intUTMPlength) == intUTMPlength)
+        printf("%-8s %-8s %d (%s)\n", currentRecord.ut_user,
+                currentRecord.ut_line, currentRecord.ut_tv.tv_sec, currentRecord.ut_host);
+
+    close(intUTMPfile);
+    return 0;
+
+
+}
