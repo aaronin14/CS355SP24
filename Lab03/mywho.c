090a117c72726cc48de9d5b700839a63ea49e117
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-27 22:54:50 -0500

    Lab03

--- Lab03/mywho.c
+++ Lab03/mywho.c
@@ -1,20 +1,27 @@
-#include <stdio.h>
 #include <fcntl.h>
-#include <utmp.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <utmp.h>
+
+void printInfo(int );
 
 int main() {
-    int intUTMPfile;
     struct utmp currentRecord;
     int intUTMPlength = sizeof(currentRecord);
+    int intUTMPfile;
     intUTMPfile = open(UTMP_FILE, O_RDONLY);
-
-    while (read(intUTMPfile, &currentRecord, intUTMPlength) == intUTMPlength)
-        printf("%-8s %-8s %d (%s)\n", currentRecord.ut_user,
-                currentRecord.ut_line, currentRecord.ut_tv.tv_sec, currentRecord.ut_host);
-
+    while (read(intUTMPfile, &currentRecord, intUTMPlength) == intUTMPlength) {
+        printf("%-4s\t %-8s\t %d (%s)\n", currentRecord.ut_user, currentRecord.ut_line,
+                currentRecord.ut_tv.tv_sec, currentRecord.ut_host);
+        printInfo(currentRecord.ut_tv.tv_sec);
+    }
     close(intUTMPfile);
     return 0;
+}
 
-
+void printInfo(int t) {
+    int d = t / 86400;
+    printf("%d ", d);
+    int y = d / 365;
+    printf("%d ", y);
 }
