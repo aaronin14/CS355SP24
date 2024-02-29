76db3b4960a82c66dd8e4f30ce2c80ecaec889c6
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-29 09:04:16 -0500

    Lab03 Completed?

--- Lab03/mywho.c
+++ Lab03/mywho.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <utmp.h>
 
-void printInfo(int );
+void printTime(long *);
 
 int main() {
     struct utmp currentRecord;
@@ -13,15 +13,7 @@ int main() {
     while (read(intUTMPfile, &currentRecord, intUTMPlength) == intUTMPlength) {
         printf("%-4s\t %-8s\t %d (%s)\n", currentRecord.ut_user, currentRecord.ut_line,
                 currentRecord.ut_tv.tv_sec, currentRecord.ut_host);
-        printInfo(currentRecord.ut_tv.tv_sec);
     }
     close(intUTMPfile);
     return 0;
-}
-
-void printInfo(int t) {
-    int d = t / 86400;
-    printf("%d ", d);
-    int y = d / 365;
-    printf("%d ", y);
 }
