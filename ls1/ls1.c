f5d75730281693a32f5419007f345cc4681757fb
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-03-04 18:37:52 -0500

    [ls1] reviewing the lecture

--- ls1/ls1.c
+++ ls1/ls1.c
@@ -1,14 +1,28 @@
+/*
+A very basic implementation of ls:
+list the names of all directory entries
+
+Pseudocode:
+    open directory
+    while not end of directory
+        read directory entry
+        display entry info
+    close directory    
+*/
+
 #include <stdio.h>
 #include <dirent.h>
 
+// list all entries of the given directory
 void do_ls(char *dir_name){
-    printf("Directory: %s\n", dir_name);
-    DIR *dir_ptr;
-    struct dirent *dirent_ptr;
+    DIR *dir_ptr;                   // pointer to a directory structure
+    struct dirent *dirent_ptr;      // pointer to the current directory entry
     dir_ptr = opendir(dir_name);
-    if(dir_ptr == 0) {
-        perror(dir_name);
-    } else {
+    // The line below is equivalent to this
+    // if (!dir_ptr==0) {
+    if(dir_ptr == 0) {              // opendir could fail, possibly because
+        perror(dir_name);           // the directory does not exist
+    } else {                        // iterate over all directory entries
         while ((dirent_ptr=readdir(dir_ptr))!=0) {
             printf("Directory entry: %s\n",dirent_ptr->d_name); 
         }
@@ -17,6 +31,6 @@ void do_ls(char *dir_name){
 }
 
 int main() {
-    do_ls(".");
+    do_ls(".");                     // list the contens of the current directory
     return 0;
 }
