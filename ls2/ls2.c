a5ba451f413ca21d89e75d9708988972aa439b16
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-03-20 13:14:00 -0400

    Added comments

--- ls2/ls2.c
+++ ls2/ls2.c
@@ -1,29 +1,54 @@
+/* 
+ * A simple implementation of ls with the long listing format (-l)
+ */
+
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <string.h>
 
+/* bitwise operators
+ * a:     0001 1101
+ * b:     0010 1011
+ *
+ * a&b:   0000 1001
+ * a|b:   0011 1111
+ *
+ * permission bits in the file mode
+ * read:  0000 0100
+ * write: 0000 0010
+ * exec:  0000 0001
+ *
+ * rwx    0000 0111
+ *
+ * user rwx    0000 0001 1100 0000
+ * group rwx   0000 0000 0011 1000
+ * world rwx   0000 0000 0000 0111
+ */
+
+// convert file mode into a printable string
 void mode_to_string(int mode, char str[]){
     strcpy(str, "----------");
-    if (S_ISDIR(mode)) str[0]='d';
-    if (S_ISCHR(mode)) str[0]='c';
-    if (S_ISBLK(mode)) str[0]='b';
-    if (S_ISLNK(mode)) str[0]='l';
+    if (S_ISDIR(mode)) str[0]='d'; // directory?
+    if (S_ISCHR(mode)) str[0]='c'; // terminal?
+    if (S_ISBLK(mode)) str[0]='b'; // disk?
+    if (S_ISLNK(mode)) str[0]='l'; // link?
 
-    if (mode & S_IRUSR) str[1]='r';
+    if (mode & S_IRUSR) str[1]='r'; // bits for the user
     if (mode & S_IWUSR) str[2]='w';
     if (mode & S_IXUSR) str[3]='x';
 
-    if (mode & S_IRGRP) str[4]='r';
+    if (mode & S_IRGRP) str[4]='r'; // bits for the group
     if (mode & S_IWGRP) str[5]='w';
     if (mode & S_IXGRP) str[6]='x';
     
-    if (mode & S_IROTH) str[7]='r';
+    if (mode & S_IROTH) str[7]='r'; // bits for the others
     if (mode & S_IROTH) str[8]='w';
     if (mode & S_IXOTH) str[9]='x';
 }
 
+// display stat information for a single file
 void show_dir_entry(struct stat *info, char *dir_entry_name) {
     char modestring[11];
     mode_to_string(info->st_mode, modestring);
@@ -34,6 +59,7 @@ void show_dir_entry(struct stat *info, char *dir_entry_name) {
     printf("%s\n", dir_entry_name);
 }
 
+// simple wrapper around stat() for error checking and printing file info
 void do_stat(char *dir_entry_name) {
     struct stat info;
     if (stat(dir_entry_name, &info)==-1)
