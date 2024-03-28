02197e785b85b6496b7467018d91d6bc274b8004
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-03-27 23:18:13 -0400

    Done Lab06

--- Lab06/myls2.c
+++ Lab06/myls2.c
@@ -1,15 +1,26 @@
+#include <asm-generic/ioctls.h>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
 int flagA=0, flagR=0, flagS=0;
 #define MaxWords 100
 #define MaxWordLength 50
+
+int get_screen_col() {
+    struct winsize wbuf;
+
+    if (ioctl(0, TIOCGWINSZ, &wbuf) != -1) 
+       return wbuf.ws_col;
+    return 0;
+}
 
 int myStringCompare(const void *a, const void *b) {
     return strcmp((const char*)a,(const char*)b);
@@ -19,42 +30,76 @@ int myStringCompare_reverse(const void *a, const void *b) {
     return strcmp((const char*)b,(const char*)a);
 }
 
-// sort and print entries based on flags
-void print_ls(char file_names[MaxWords][MaxWordLength], int arr_length) {
+int remove_hidden_files(char fileNames[MaxWords][MaxWordLength], int numberOfFile) {
+    for(int i=0; i<numberOfFile; i++) {
+        if(fileNames[i][0]=='.') {
+            strcpy(fileNames[i],fileNames[--numberOfFile]);
+            strcpy(fileNames[numberOfFile],"");
+        }
+    }
+    return numberOfFile;
+}
+
+void print_ls(char fileNames[MaxWords][MaxWordLength], int numberOfFile, int maxNameLength) {
+    if(flagA==0)
+        numberOfFile=remove_hidden_files(fileNames, numberOfFile);
+    // Sorting the fileNames
     if(flagS==1) {
-        qsort(file_names, arr_length, MaxWordLength, myStringCompare);
+        qsort(fileNames, numberOfFile, MaxWordLength, myStringCompare);
     }
     if(flagR==1) {
-        qsort(file_names, arr_length, MaxWordLength, myStringCompare_reverse);
+        qsort(fileNames, numberOfFile, MaxWordLength, myStringCompare_reverse);
     }
-    for(int i=0; i<arr_length; i++) {
-        if(flagA==1) {
-            printf("%s\n",file_names[i]);
-        } else {
-            if(file_names[i][0]!='.')
-                printf("%s\n",file_names[i]);
+
+    // Calculating number of Column and Row needed to print fileNames
+    int termColumns=get_screen_col();
+    int col=termColumns/(maxNameLength+2);
+    int row=1;
+    int remainder=0;
+
+    if(numberOfFile>col) {
+        row=numberOfFile/col;
+        remainder=numberOfFile%col;
+    } else
+        col=numberOfFile;
+
+    if(remainder>0) {
+        row++;
+        col=(numberOfFile/row)+1;
+    }
+
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            int index=i+(j*row);
+            printf("%-*s  ", maxNameLength, fileNames[index]);
         }
+        printf("\n"); 
     }
 }
 
-// get all entries an store them in file_names array
+// get all entries an store them in fileNames array
 void do_ls(char *dir_name) {
-    char file_names[MaxWords][MaxWordLength];
+    char fileNames[MaxWords][MaxWordLength];
     DIR *dir_ptr;
     struct dirent *dirent_ptr;
     dir_ptr = opendir(dir_name);
+   
+    int maxNameLength=0;
 
     if (dir_ptr==0) {
         perror(dir_name);
     } else {
         int i=0;
+        // Get the max length of a file name
         while ((dirent_ptr=readdir(dir_ptr))!=0 && i<MaxWords) {
-            strcpy(file_names[i], dirent_ptr->d_name);
+            strcpy(fileNames[i], dirent_ptr->d_name);
+            if (maxNameLength<strlen(fileNames[i]))
+                maxNameLength=strlen(fileNames[i]);
             i++;
         }
     closedir(dir_ptr);
 
-    print_ls(file_names,i);
+    print_ls(fileNames,i,maxNameLength);
     }
 }
 
