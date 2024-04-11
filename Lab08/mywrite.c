f1a61d7d592e29e3669dec9e048f402e2b48f14a
Aaron Hong Nguyen <aaron14nguyen@gmail.com>
2024-04-11 01:04:11 -0400

    wrapper around strcmp

--- Lab08/mywrite.c
+++ Lab08/mywrite.c
@@ -1,13 +1,3 @@
-/*
- * Working scaffolding for the write system command
- *
- * Ctrl-D sends EOF character
- * Ctrl-Z suspends the process
- *      use ps to see the lis of all processes and get pid of a suspended processes
- *      use kill (or kill -KILL) to terminate the suspended process, if needed
- *      you may also try using top or htop commands to see the list of running processes
- */
-
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -16,14 +6,19 @@
 
 #define BUFSIZE 1024
 
+int my_strcmp(char *str1, char *str2) {
+    return strcmp(str1, str2);
+}
+
 int main(int ac, char* av[]) {
     if(ac!=2) 
         fprintf(stderr, "Usage: %s username\n", av[0]);
     else {
+	char *username = av[1];
 
         // Open UTMP file to check if username is valid and find the user's tty path
-        struct utmp currentRecord;
-        int intUTMPlength = sizeof(currentRecord);
+        struct utmp utmp_entry;
+        int intUTMPlength = sizeof(utmp_entry);
         int utmp_fd;
         if((utmp_fd = open(UTMP_FILE, O_RDONLY))==-1) {
             printf("Error: Unable to use the UTMP file");
@@ -31,31 +26,31 @@ int main(int ac, char* av[]) {
         }
 
         // Iterating through each utmp record
-        while(read(utmp_fd, &currentRecord, intUTMPlength)==intUTMPlength) {
+        while(read(utmp_fd, &utmp_entry, intUTMPlength)==intUTMPlength) {
 
             // Check if the user exists and the record is a user process
-            //if((currentRecord.ut_type == USER_PROCESS) && (strcmp(currentRecord.ut_user,av[1])==0)) {
-            if(currentRecord.ut_type == USER_PROCESS) {
+            if(utmp_entry.ut_type == USER_PROCESS && my_strcmp(utmp_entry.ut_user, username)==0) {
+                printf("%s %d", utmp_entry.ut_user, utmp_entry.ut_type);
 
                 // Get the tty path and open it
-                char ttyPath[50];
-                sprintf(ttyPath, "/dev/%s", currentRecord.ut_line);
-                int tty_fd=open(ttyPath, O_WRONLY);   
+                char tty_path[50];
+                snprintf(tty_path, 50, "/dev/%s", utmp_entry.ut_line);
+                int tty_fd=open(tty_path, O_WRONLY);   
 
                 if(tty_fd==-1) {
-                    perror(ttyPath);
+                    perror(tty_path);
                     close(utmp_fd);
                     return 1;
-                } else {
-                    char buf[BUFSIZE];
-                    char hiMessage[]="Message another termimal...\n";
-                    char byeMessage[]="EOF";
-                    while(fgets(buf, BUFSIZE, stdin)!=0)    // read a string from the current terminal 
-                        if(write(tty_fd, buf, strlen(buf))==-1) // and write it to the target terminal
+
+                } 
+                char buf[BUFSIZE];
+                char hiMessage[]="Message another termimal...\n";
+                char byeMessage[]="EOF";
+                while(fgets(buf, BUFSIZE, stdin)!=0)    // read a string from the current terminal 
+                	if(write(tty_fd, buf, strlen(buf))==-1) // and write it to the target terminal
                             break;
-                    write(tty_fd, byeMessage, strlen(byeMessage));
-                    close(tty_fd); 
-                }
+                write(tty_fd, byeMessage, strlen(byeMessage));
+                close(tty_fd); 
             }
         }
         close(utmp_fd);
