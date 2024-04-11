edc56f2e6fe5d34642a33f379465ae2c6a46f130
Aaron Hong Nguyen <aaron14nguyen@gmail.com>
2024-04-11 04:43:49 +0000

    50%

--- Lab08/mywrite.c
+++ Lab08/mywrite.c
@@ -12,27 +12,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <utmp.h>
 
 #define BUFSIZE 1024
 
 int main(int ac, char* av[]) {
-    if(ac!=2)   // the program requires one parameter: the target terminal name
-                // you may need to add /dev before ttyname, e.g. /dev/pts/1
-        fprintf(stderr, "usage: %s ttyname\n", av[0]);
+    if(ac!=2) 
+        fprintf(stderr, "Usage: %s username\n", av[0]);
     else {
-        int fd=open(av[1], O_WRONLY);   // open the target terminal for writing
-        if(fd==-1)                      // what if it could not be opened
-            perror(av[1]);
-        else {
-            char buf[BUFSIZE];
-            char hiMessage[]="Message another ternimal...\n";
-            char byeMessage[]="EOF";
-            while(fgets(buf, BUFSIZE, stdin)!=0)    // read a string from the current terminal 
-                if(write(fd, buf, strlen(buf))==-1) // and write it to the target terminal
-                    break;
-            write(fd, byeMessage, strlen(byeMessage));
-            close(fd);
+
+        // Open UTMP file to check if username is valid and find the user's tty path
+        struct utmp currentRecord;
+        int intUTMPlength = sizeof(currentRecord);
+        int utmp_fd;
+        if((utmp_fd = open(UTMP_FILE, O_RDONLY))==-1) {
+            printf("Error: Unable to use the UTMP file");
+            return 1;
         }
+
+        // Iterating through each utmp record
+        while(read(utmp_fd, &currentRecord, intUTMPlength)==intUTMPlength) {
+
+            // Check if the user exists and the record is a user process
+            //if((currentRecord.ut_type == USER_PROCESS) && (strcmp(currentRecord.ut_user,av[1])==0)) {
+            if(currentRecord.ut_type == USER_PROCESS) {
+
+                // Get the tty path and open it
+                char ttyPath[50];
+                sprintf(ttyPath, "/dev/%s", currentRecord.ut_line);
+                int tty_fd=open(ttyPath, O_WRONLY);   
+
+                if(tty_fd==-1) {
+                    perror(ttyPath);
+                    close(utmp_fd);
+                    return 1;
+                } else {
+                    char buf[BUFSIZE];
+                    char hiMessage[]="Message another termimal...\n";
+                    char byeMessage[]="EOF";
+                    while(fgets(buf, BUFSIZE, stdin)!=0)    // read a string from the current terminal 
+                        if(write(tty_fd, buf, strlen(buf))==-1) // and write it to the target terminal
+                            break;
+                    write(tty_fd, byeMessage, strlen(byeMessage));
+                    close(tty_fd); 
+                }
+            }
+        }
+        close(utmp_fd);
     }
     return 0;
 }
